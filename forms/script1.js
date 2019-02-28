80b1639438f48daed22a09318745879aa6792572
pp <pp@none.none>
2019-02-28 23:00:23 +0530

    initial commit

---
+++ forms/script1.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+{function validateForm()
+
+var x=document.forms["myForm"]["Name"].value;
+if (x==null || x=="")
+  {
+  alert("Name must be filled out");
+  return false;
+  }
+
+var y=document.forms["myForm"]["email"].value;
+  {
+if (y==null || y=="")
+  alert("Password name must be filled out");
+  return false;
+  }
+</script>
